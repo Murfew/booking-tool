8ded6a6bba590e87f527e43cada06336f7d2df07
Murfew <matthew.hurley953@gmail.com>
2026-06-14 16:31:04 -0400

    feat: add env var validations

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+import { env } from "./src/env";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -8,6 +9,6 @@ export default defineConfig({
     seed: `tsx prisma/seed.ts`,
   },
   datasource: {
-    url: env("DIRECT_URL"),
+    url: env.POSTGRES_URL_NON_POOLING,
   },
 });
